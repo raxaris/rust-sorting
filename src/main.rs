898a8f57cfceba3438e730da1077675cfb4a5102
Ansar <121044855+raxaris@users.noreply.github.com>
2024-04-20 11:29:08 +0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
 extern crate sortingLibrary;
 use sortingLibrary::{merge_sort, quick_sort, insertion_sort, selection_sort};
 fn main() {
-    let mut numbers: Vec<i32> = vec![232, 111, 23, 9999, 1234, 1, 9, -7182, -14, 333];
+    let mut test: Vec<i32> = vec![232, 111, 23, 9999, 1234, 1, 9, -7182, -14, 333];
     println!("Original: {:?}", numbers);
 
-    quick_sort(&mut numbers);
-    println!("Quick Sorted: {:?}", numbers);
+    quick_sort(&mut test);
+    println!("Quick Sort Result {:?}", test);
 
-    let mut numbers2 = vec![232, 111, 23, 9999, 1234, 1, 9, -7182, -14, 333];
-    selection_sort(&mut numbers2);
-    println!("Selection Sorted: {:?}", numbers2);
+    let mut test2 = vec![232, 111, 23, 9999, 1234, 1, 9, -7182, -14, 333];
+    selection_sort(&mut test2);
+    println!("Selection Sort Result {:?}", test2);
 
-    let mut numbers3: Vec<i32> = vec![232, 111, 23, 9999, 1234, 1, 9, -7182, -14, 333];
-    insertion_sort(&mut numbers3);
-    println!("Insertion Sorted: {:?}", numbers3);
+    let mut test3: Vec<i32> = vec![232, 111, 23, 9999, 1234, 1, 9, -7182, -14, 333];
+    insertion_sort(&mut test3);
+    println!("Insertion Sort Result{:?}", test3);
 
-    let mut numbers4 = vec![232, 111, 23, 9999, 1234, 1, 9, -7182, -14, 333];
-    merge_sort(&mut numbers4);
-    println!("Merge Sorted: {:?}", numbers4);
+    let mut test4 = vec![232, 111, 23, 9999, 1234, 1, 9, -7182, -14, 333];
+    merge_sort(&mut test4);
+    println!("Merge Sort Result {:?}", test4);
 }
